3d1e13081637a2cc75f7d9e78d99a017b1a3a390
dyvon <dyvonhill2@gmail.com>
2016-05-01 17:22:49 -0700

    select_change() not working,,,,,, setAttribute class works but not ng-model/ng-options.

--- app/module.js
+++ app/module.js
@@ -3,16 +3,8 @@ angular.module('app', [])
   .service('SkinsApi', skinsApi);
 
 var select_skin_type = document.getElementById("select_skin_types");
-var skin_list_div = document.getElementById("skin_list_div");
-var select = document.createElement("select");
-var att1 = document.createAttribute("class");
-att1.value ="form-control";
-var att2 = document.createAttribute("ng-model");
-var att3 = document.createAttribute("ng-options");
-/**
- * the Api service will get data via http requests
- * and make it available inside our application.
- */
+
+
 function skinsApi($http) {
   /**
    * This function will call an api and return some skins.
@@ -70,6 +62,16 @@ function skinsApi($http) {
     };
 }
 
+function select_change(){
+    var skin_list_div = document.getElementById("skin_list_div");
+    var select = document.createElement("select");
+    skin_list_div.appendChild(select);
+    select.setAttribute("class", "form-control");
+    select.setAttribute("ng-model", "app.selectedSkin");
+    select.setAttribute("ng-options", "skin.name as skin.name + ' - ' + skin.currencyValue for skin in app.skins");
+
+
+}
 /**
  * Skins Controller.
  *
@@ -77,52 +79,29 @@ function skinsApi($http) {
  * @constructor
  */
 
-function select_change(){
-    if(select_skin_type.value == 1){
-        att2.value=
-        att3.value=
-       skin_list_div.appendChild(select);
-    }
-    if(select_skin_type.value == 2){
-
-    }
-    if(select_skin_type.value == 3){
-
-    }
-    if(select_skin_type.value == 4){
-
-    }
-    if(select_skin_type.value == 5){
-
-    }
-    if(select_skin_type.value == 6){
-
-    }
-    if(select_skin_type.value == 7){
-
-    }
-    if(select_skin_type.value == 8){
-
-    }
-
-}
-
 function AppCtrl(SkinsApi, $filter) {
+        var test = SkinsApi.rifle()
+            .then(function (skins) {
+                var names = _.keys(skins),
+                    data = _.values(skins);
+
+                data = _.map(data, function (skin, index) {
+                    skin.name = names[index];
+                    skin.last_updated = moment(skin.last_updated * 1000).fromNow();
+                    skin.currencyValue = $filter('currency')(skin.value / 100);
+                    return skin;
+                });
+
+                vm.skins = data;
+            });
+
+
   var vm = this;
-    console.time('loading');
-  SkinsApi.knife()
-    .then(function (skins) {
-      var names = _.keys(skins),
-        data = _.values(skins);
-
-      data = _.map(data, function (skin, index) {
-        skin.name = names[index];
-        skin.last_updated = moment(skin.last_updated * 1000).fromNow();
-          skin.currencyValue = $filter('currency')(skin.value / 100);
-        return skin;
-      });
-
-      vm.skins = data;
-        console.timeEnd('loading');
-    });
+  test
 }
+
+
+
+
+
+
