37d8fd9451dd47a320780933dcd6ce6560c91d68
dyvon <dyvonhill2@gmail.com>
2016-05-12 23:53:38 -0700

    Almossssssst

---
+++ app/js.js
@@ -0,0 +1,11 @@
+var sniper  = document.getElementById('sniper_id').value;
+var rifle = document.getElementById('rifle_id').value;
+var submachine = document.getElementById('submachine_id').value;
+var lightmachine = document.getElementById('lightmachine_id').value;
+var pistol = document.getElementById('pistol_id').value;
+var knife = document.getElementById('knife_id').value;
+var shotgun = document.getElementById('shotgun_id').value;
+var misc = document.getElementById('misc_id').value;
+
+var li = document.getElementsByName("li");
+var active = document.getElementsByClassName("active");
